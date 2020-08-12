88dd94872f84753642dec28c1e18437ec1ab7858
Hritvik Agarwal <65281768+Hritvik-55@users.noreply.github.com>
2020-08-12 15:23:19 +0530

    Delete Rotate_Arrays.py

--- Rotate_Arrays.py
+++
@@ -1,9 +0,0 @@
-def func(lst,k):
-    newlst=[]
-    for i in range(len(lst)-k,len(lst)):
-        newlst.append(lst[i])
-    for i in range(0,len(lst)-k):
-        newlst.append(lst[i])
-    return newlst
-arr=[1,2,3,4,45,6,74]
-print(func(arr,3))
