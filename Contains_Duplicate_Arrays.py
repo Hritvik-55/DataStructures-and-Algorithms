e79d67a553700a37146067aff2771720a9c9d08d
Hritvik Agarwal <65281768+Hritvik-55@users.noreply.github.com>
2020-08-12 15:21:53 +0530

    Delete Contains_Duplicate_Arrays.py

--- Contains_Duplicate_Arrays.py
+++
@@ -1,9 +0,0 @@
-def func(lst):
-    for i in range(len(lst)):
-        for j in range(i+1,len(lst)):
-            if lst[i]==lst[j]:
-                return "Duplicate Item Found"
-            else:
-                return "All the elements are distinct"
-arr=[1,6,5,2]
-print(func(arr))
